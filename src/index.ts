7c88fcc32b28d628d9f75d06884fee7a31af7295
Simon DUJARDIN <you@example.com>
2020-01-06 16:16:45 +0100

    :sparkles: add prom middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
 import "reflect-metadata";
-import { buildSchema } from "type-graphql";
+import { buildSchema, MiddlewareFn } from "type-graphql";
 import ProjectResolver from "./resolvers/ProjectResolver";
 import TaskResolver from "./resolvers/TaskResolver";
 import * as mongoose from "mongoose";
 import { ObjectId } from "mongodb";
 import { ObjectIdScalar } from "./object-id.scalar";
+import { prometheus, serverPrometheus } from "./graphql-middleware-prometheus";
 
 mongoose.connect("mongodb://localhost:27017/taskManager", {
   useNewUrlParser: true
@@ -17,11 +18,12 @@ async function bootstrap() {
     emitSchemaFile: true,
     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }]
   });
-
+  const prometheusMiddleware = prometheus({});
   const server = new GraphQLServer({
-    schema
+    schema,
+    middlewares: [prometheusMiddleware]
   });
-
+  serverPrometheus(server);
   server.start(() => console.log("Server is running on http://localhost:4000"));
 }
 
